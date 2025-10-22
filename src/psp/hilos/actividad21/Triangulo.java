3c5914617c01299f1bc0422651eceba873ca8132
Wasima03 <elouastaniaznag@gmail.com>
2025-10-22 12:06:32 +0200

    22/10

--- src/psp/hilos/actividad21/Triangulo.java
+++ src/psp/hilos/actividad21/Triangulo.java
@@ -1,4 +1,21 @@
 package psp.hilos.actividad21;
 
-public class Triangulo {
+public class Triangulo extends  Thread{
+
+    private float altura;
+    private float base;
+    float aux=0;
+
+    public Triangulo(float altura,float base) {
+        this.altura = altura;
+        this.base=base;
+    }
+
+    public void run(){
+        for(int i=0;i<base;i++){
+            aux+=altura;
+        }
+        System.out.println("Área del "+ this.getName()+ " con "+altura+" de altura y "+base+" de base : "+aux/2);
+
+    }
 }
--- src/psp/hilos/actividad21/main.java
+++ src/psp/hilos/actividad21/main.java
@@ -1,15 +1,19 @@
 package psp.hilos.actividad21;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        List<Triangulo> hilos=new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+        int prioridad=0;
         System.out.print("¿Cuántos triángulos quieres calcular? ");
         int num=sc.nextInt();
+        sc.nextLine();
         System.out.print("¿Quiere asignar prioridad al cálculo de los triángulos? (S/N)");
         String opc = sc.nextLine();
-        sc.nextLine();
         int contador=0;
         System.out.println();
         while (contador!=num){
@@ -18,19 +22,33 @@ public class main {
             float base= sc.nextFloat();
             System.out.print("Introduce la altura: ");
             float altura= sc.nextFloat();
-            if (opc.equalsIgnoreCase("s"));{
+            if (opc.equalsIgnoreCase("s")){
                 System.out.print("Introduce la prioridad del 1 al 10: ");
                 try{
-                    int pr=sc.nextInt();
-
+                    prioridad=sc.nextInt();
+                    hilos.add(new Triangulo(altura,base));
 
                 }catch (NumberFormatException e){
                     e.printStackTrace();
                 }
+            }else{
+                hilos.add(new Triangulo(altura,base));
             }
-
             contador+=1;
 
         }
+        int n=1;
+        for(Triangulo t : hilos){
+            t.setName("Triángulo "+n);
+            if (opc.equalsIgnoreCase("s")){
+                t.setPriority(prioridad);
+            }
+            t.start();
+            n+=1;
+        }
+        for(Triangulo t : hilos){
+            t.join();
+        }
+        System.out.println("Fin del programa...");
     }
 }
