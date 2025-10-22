02a52d810c842ec7da87235be24ae752f8dfc1df
Wasima03 <elouastaniaznag@gmail.com>
2025-10-22 12:06:42 +0200

    22/10

---
+++ src/psp/hilos/actividad21/prueba.java
@@ -0,0 +1,13 @@
+package psp.hilos.actividad21;
+
+public class prueba {
+    public static void main(String[] args) {
+        int base=4;
+        int altura=3;
+        int aux=0;
+        for(int i=0;i<base;i++){
+            aux+= altura;
+        }
+        System.out.println(aux);
+    }
+}
